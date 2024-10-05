a77ee2c7b6cbb7c5b1e59628e954e7534321f102
Mark Veskov <mark.veskov@abv.bg>
2024-10-05 13:08:08 +0300

    triangle area exercise

---
+++ Equations/equation_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*
+  1. Пресметнете периметъра и лицето на триъгълник със страни 10, 7, 6.5.
+    1.1. Въвеждайте трите страни от стандартния вход. Приемете засега, че всеки триъгълник, който въвеждате, е валиден (сборът на всеки 2 страни да е по-голям от третата).
+*/
+
+int main()
+{
+  double a = 10, b = 6, c = 6.5;
+
+  double semiPerimeter = (a + b + c) / 2;
+  double area = sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+
+  cout << "Area of triangle is: " << area << endl;
+
+  return 0;
+}
