ee8db466129fd1f050f9373ca029b7cb6efcb264
Mark Veskov <mark.veskov@abv.bg>
2024-10-05 15:59:39 +0300

    Id encryption

---
+++ Equations/equation_7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string ID;
+
+  do
+  {
+    cout << "Enter your ID (10 chars): ";
+    cin >> ID;
+
+    if (ID.length() != 10)
+    {
+      cout << "Please enter a valid ID (10 chars)" << endl;
+    }
+
+  } while (ID.length() != 10);
+
+  for (int i = 0; i < 7; i++)
+  {
+    ID.replace(i, 1, "*");
+  }
+
+  cout << "Your encrypted ID: " << ID << endl;
+
+  return 0;
+}
