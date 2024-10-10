7cd39957bd9ccf607d4ae2568d7ad26df04bc801
Mark Veskov <mark.veskov@abv.bg>
2024-10-10 21:04:29 +0300

    Task 3

---
+++ GROUP_1_WEEK_1/task3.cpp
@@ -0,0 +1,25 @@
+// Task 3 - Bigger number
+// Въведете две цели числа и изведете в конзолата по - голямото от двете (без if else проверка тъй като не е учено още :) )
+// Примерен изход при въведени числа 10 и 15 : 15 10
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  int a, b;
+
+  cout << "Enter two integers: ";
+  cin >> a >> b;
+
+  // Изчисляване на по-голямото число
+  int maxNumber = (a + b + abs(a - b)) / 2;
+
+  // Изчисляване на по-малкото число
+  int minNumber = (a + b - abs(a - b)) / 2;
+
+  cout << maxNumber << " " << minNumber << endl;
+
+  return 0;
+}
