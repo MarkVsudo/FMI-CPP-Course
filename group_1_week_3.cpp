1d8276e481ea581d2ef0a3b17004289220c4423c
Mark Veskov <119725972+MarkVsudo@users.noreply.github.com>
2024-10-22 14:32:53 +0300

    Add files via upload

---
+++ group_1_week_3.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Task 0
+    // int sum = 0;
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     int num;
+    //     cin >> num;
+    //     sum += num;
+    // }
+    // cout << "Sum is: " << sum;
+
+    // Task 0.5
+
+    // double number;
+
+    // cin >> number;
+
+    // if (number < 0.5)
+    // {
+    //     cout << "Enter a positive number!";
+    //     return 1;
+    // }
+
+    // int lengthLoop = number * 2;
+
+    // for (int i = 0; i <= lengthLoop; i++)
+    // {
+    //     if (number >= 1)
+    //     {
+    //         number -= 0.5;
+    //         cout << number - 0.5 << " ";
+    //     }
+    // }
+
+    // Task 1
+
+    // int number;
+
+    // cout << "Enter a number: ";
+    // cin >> number;
+
+    // int factorial = 1;
+    // for (int i = 1; i <= number; i++)
+    // {
+    //     factorial *= i;
+    // }
+    // cout << factorial;
+
+    // Task 2
+
+    // int number;
+    // int sum = 0;
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     cin >> number;
+    //     sum += number;
+    //     if (number == 0)
+    //     {
+    //         cout << "Sum: " << sum;
+    //         return 1;
+    //     }
+    // }
+
+    // Task 3
+
+    // int number;
+    // cout << "Enter a number: ";
+    // cin >> number;
+
+    // Task 4
+
+    // int num;
+    // cout << "Enter a number: ";
+    // cin >> num;
+
+    // if (num % 2 != 0)
+    //     cout << "YES";
+    // else
+    //     cout << "NO";
+
+    // Task 5
+
+    // 1, 1, 2, 3, 5, 8, 13, 21
+    // 0, 1, 1, 2, 3, 5, 8, 13
+
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // int first = 0, second = 1, third;
+    // int temp;
+    // for (int i = 1; i < n; i++)
+    // {
+    //     third = first + second;
+    //     first = second;
+    //     second = third;
+    // }
+
+    // cout << "Third number: " << third;
+
+    // Task 6
+
+    // int a, b, result = 1;
+
+    // cout << "Enter two numbers: ";
+
+    // cin >> a >> b;
+
+    // for (int i = 0; i < b; i++)
+    // {
+    //     result *= a;
+    // }
+
+    // cout << "Result: " << result;
+
+    // Task 7
+
+    return 0;
+}
